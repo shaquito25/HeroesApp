e21607fd20e1a4424f4620498dd325e6a40f0538
Arturo <arturogabriel25@gmail.com>
2021-06-17 14:18:50 -0400

    cambios en build

---
+++ docs/precache-manifest.0af9b40404dd2643771ec97fcb51de6e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac693e9e417ce567c844d73674bf3bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db980b48f5ee70f9ce8",
+    "url": "/static/js/2.e068106a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e068106a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746698784bd806b74c76",
+    "url": "/static/js/main.48347896.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  },
+  {
+    "revision": "a179d8711d59015e5d3d53637ad9fce7",
+    "url": "/static/media/dc-arrow.a179d871.jpg"
+  },
+  {
+    "revision": "8efedfa7bf353070fb1dc56bc36a8bd3",
+    "url": "/static/media/dc-batman.8efedfa7.jpg"
+  },
+  {
+    "revision": "5d203be297ee7aee6df388f48effc2d2",
+    "url": "/static/media/dc-black.5d203be2.jpg"
+  },
+  {
+    "revision": "871a00dda57a453a698369e1687db44e",
+    "url": "/static/media/dc-blue.871a00dd.jpg"
+  },
+  {
+    "revision": "d22cd5d0cdbb8e6cb64ea844fd1082fa",
+    "url": "/static/media/dc-flash.d22cd5d0.jpg"
+  },
+  {
+    "revision": "dae0db0a19c068035a119a4cf07bfd39",
+    "url": "/static/media/dc-green.dae0db0a.jpg"
+  },
+  {
+    "revision": "9629381c9ae8fe6a5c9ab23308cd76f0",
+    "url": "/static/media/dc-martian.9629381c.jpg"
+  },
+  {
+    "revision": "3b3b7dd1a4716a9f8eec3c13c0611726",
+    "url": "/static/media/dc-robin.3b3b7dd1.jpg"
+  },
+  {
+    "revision": "ad88dd859f1b6cb05d235edfdf7d2da9",
+    "url": "/static/media/dc-superman.ad88dd85.jpg"
+  },
+  {
+    "revision": "b01172005be3eba0ddbe50bd07d3ece8",
+    "url": "/static/media/dc-wonder.b0117200.jpg"
+  },
+  {
+    "revision": "71c5a519e43cab5350b5a5dbb45f4ece",
+    "url": "/static/media/marvel-captain.71c5a519.jpg"
+  },
+  {
+    "revision": "017af60c9d646181629f0887c02e8ef0",
+    "url": "/static/media/marvel-cyclops.017af60c.jpg"
+  },
+  {
+    "revision": "3d4c3df826dce8f0a85e8e49bd3fb204",
+    "url": "/static/media/marvel-daredevil.3d4c3df8.jpg"
+  },
+  {
+    "revision": "df41e28ce57042715584e972ea53e5a2",
+    "url": "/static/media/marvel-hawkeye.df41e28c.jpg"
+  },
+  {
+    "revision": "8f5cd9397468b7b508ffa33b00c5014e",
+    "url": "/static/media/marvel-hulk.8f5cd939.jpg"
+  },
+  {
+    "revision": "8f307eef75cd45b3e7076ece1b368e5f",
+    "url": "/static/media/marvel-iron.8f307eef.jpg"
+  },
+  {
+    "revision": "c055e4a00bff140e8faf40eee0701eaa",
+    "url": "/static/media/marvel-silver.c055e4a0.jpg"
+  },
+  {
+    "revision": "9967f816d7adb4c674f99d230e9472c7",
+    "url": "/static/media/marvel-spider.9967f816.jpg"
+  },
+  {
+    "revision": "9f1b74e51e318f14c533fdb3b03a7421",
+    "url": "/static/media/marvel-thor.9f1b74e5.jpg"
+  },
+  {
+    "revision": "a1a41d9adf2f16f1220723a70ce20215",
+    "url": "/static/media/marvel-wolverine.a1a41d9a.jpg"
+  }
+]);
